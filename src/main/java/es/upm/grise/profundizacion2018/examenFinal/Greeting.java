7be5e362569f5f22746498c2d287c74f085c24a6
Omar <omar.mokrani.gallego@alumnos.upm.es>
2021-04-15 22:41:55 +0200

    Despues de la tutoría Tutoría

--- src/main/java/es/upm/grise/profundizacion2018/examenFinal/Greeting.java
+++ src/main/java/es/upm/grise/profundizacion2018/examenFinal/Greeting.java
@@ -1,15 +1,13 @@
 package es.upm.grise.profundizacion2018.examenFinal;
 
-//import java.util.Calendar;
+import java.util.Calendar;
 
 public class Greeting {
 	
-	public String getGreeting( Language language ) {
+	public String getGreeting( Language language, Calendar calendar ) {
 		// Get current hour
-		/*Calendar calendar = Calendar.getInstance();
-		int hour = calendar.get( Calendar.HOUR_OF_DAY );*/
-		
-		MyCalendar calendar = new MyCalendar();
+		/*Calendar calendar = Calendar.getInstance();*/
+		int hour = calendar.get( Calendar.HOUR_OF_DAY );
 		
 		// Find out the greeting language
 		if ( language == null )
@@ -17,9 +15,9 @@ public class Greeting {
 		
 		// Get the moment of the day
 		TimeOfTheDay moment;
-		if( calendar.hour < 12 ) 
+		if( hour < 12 ) 
 			moment = TimeOfTheDay.MORNING;
-		else if ( calendar.hour < 18 )
+		else if ( hour < 18 )
 			moment = TimeOfTheDay.AFTERNOON;
 		else
 			moment = TimeOfTheDay.EVENING;
